1a131a070209ac9fff03f49253f43c6ead0ea946
samuel <betysam55@gmail.com>
2018-06-12 07:05:40 +0300

    project document

--- resources/views/candidates/candidatevote.blade.php
+++ resources/views/candidates/candidatevote.blade.php
@@ -1,45 +1,25 @@
 @extends('layouts.app')
-
-
 @section('content')
 <div class="bootstrap-iso container">
 <div class="container">
-
     <div class="row">
-
         <div class="col-md-12">
-
             <div class="panel panel-default">
-
                 <div class="panel-heading">Presidential Vote</div>
-
-
                 <div class="panel-body">
-
-
                     <table class="table table-bordered">
-
                         <tr>
-
                             <th>Id</th>
-
                             <th>Name</th>
                             <th>Last Name</th>
                             <th>Department</th>
                             <th width="400px">Result</th>
-
                             <th width="100px">Vote</th>
-
                         </tr>
-
                         @if($posts->count())
-                        
                             @foreach($posts as $post)
-
                             <tr>
-
                                 <td>{{ $post->id }}</td>
-
                                 <td>{{ $post->fname }}</td>
                                 <td>{{ $post->lname }}</td>
                                 <td>{{ $post->department }}</td>
@@ -49,30 +29,17 @@
                                    $id=$post->ratingPercent(100);
                                    echo $id;  ?>%">{{$post->ratingPercent(100)}}</div>
                                     </div>
-
-                                  
                                 </td>
-                                
                                 <td>
-                                    
                                     <a href="{{ route('pposts.show',$post->id) }}" class="btn btn-primary btn-sm">View</a>
                                 </td>
-
                             </tr>
-
                             @endforeach
-
                         @endif
-
                     </table>
-
-
                 </div>
-
             </div>
-
         </div>
-
     </div>
 
 </div>
